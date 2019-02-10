c719d20e4e2a23fe98cbf6c994f37053eb16a84a
ZoeBKramer <zoebkramer@gmail.com>
2019-02-10 08:08:55 +0000

    geocoding address

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,12 @@
 class Place < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 20, minimum: 3 } 
+  validates :address, presence: true, length: { maximum: 100, minimum: 5 }
+  validates :description, presence: true, length: { maximum: 140, minimum: 5 }
+
+  geocoded_by :address
+  after_validation :geocode 
+
   validates :name, presence: true
 end
